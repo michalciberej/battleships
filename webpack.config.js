e1c16947c459f01882eff4d2ea27064ca56c3aca
Michal Ciberej <michal.ciberej@gmail.com>
2023-07-04 21:56:40 +0200

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  watch: true,
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
